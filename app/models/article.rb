ea7c0bf8abecc80dd348c96d58fac9ddc310e5a4
tchikuba <chikuba.tsutomu@gmail.com>
2018-10-16 16:14:00 +0900

    meta-tagsをActiveRecordと連携して使う

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,2 +1,9 @@
 class Article < ApplicationRecord
+  def to_meta_tags
+    {
+      title: title,
+      description: body,
+      keywords: keywords
+    }
+  end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_07_22_101607) do
+ActiveRecord::Schema.define(version: 2018_10_16_064918) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -20,6 +20,7 @@ ActiveRecord::Schema.define(version: 2018_07_22_101607) do
     t.text "body"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "keywords"
   end
 
 end
